f897e2c8352f744d43a670ac23d32820a3816eb8
Sienna Kopf <princess.kopf@gmail.com>
2020-07-17 09:17:37 -0600

    Add one to many relationship between rides and amusement park

---
+++ db/migrate/20200717151008_create_amusement_park.rb
@@ -0,0 +1,10 @@
+class CreateAmusementPark < ActiveRecord::Migration[5.1]
+  def change
+    create_table :amusement_parks do |t|
+      t.string :name
+      t.integer :admission_price
+
+      t.timestamps
+    end
+  end
+end
